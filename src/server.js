885d761448af7a3c5695c22ce7386ca36e518ea5
Joe Carli <jcarli@truu.io>
2019-08-23 17:42:09 -0600

    Hacking away at server.js to learn

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const jwksClient = require('jwks-rsa');
 const request = require('request-promise');
 const session = require('express-session');
 
+const idp = 'http://localhost:9001/oidc'
+
 // loading env vars from .env file
 require('dotenv').config();
 
@@ -43,11 +45,174 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.status(501).send();
+  // define constants for the authorization request
+  //const authorizationEndpoint = oidcProviderInfo['authorization_endpoint'];
+  const authorizationEndpoint = idp + '/authorize';
+  const responseType = 'code';//'id_token';
+  const scope = 'openid email';
+  const clientID = process.env.CLIENT_ID;
+  const redirectUri = 'http://localhost:3000/callback';
+  //const responseMode = 'form_post';
+  const state = 'abc';
+  const nonce = crypto.randomBytes(16).toString('hex'); // define a signed cookie containing the nonce value
+  const options = {
+    maxAge: 1000 * 60 * 15,
+    httpOnly: true, // The cookie only accessible by the web server
+    signed: true // Indicates if the cookie should be signed
+  };
+  // add cookie to the response and issue a 302 redirecting user
+  /*res
+    .cookie(nonceCookie, nonce, options)
+    .redirect(
+      authorizationEndpoint + 
+      '?response_mode=' + responseMode + 
+      '&response_type=' + responseType + 
+      '&scope=' + scope +
+      '&client_id=' + clientID + 
+      '&redirect_uri='+ redirectUri + 
+      '&nonce='+ nonce
+    );*/
+  res.redirect(
+    authorizationEndpoint +
+    '?response_type=' + responseType +
+    '&scope=' + scope +
+    '&client_id=' + clientID +
+    '&state=' + state +
+    '&redirect_uri=' + redirectUri
+  );
 });
 
-app.post('/callback', async (req, res) => {
-  res.status(501).send();
+//app.post('/callback', async (req, res) => {
+app.get('/callback', async (req, res) => {
+
+  console.log('in callback');
+  const authCode = req.query.code;
+  const state = req.query.state;
+
+  console.log('auth code: ' + authCode);
+  console.log('state: ' + state);  
+
+  // get auth token
+  var options = {
+    method: 'POST',
+    uri: idp + '/token',
+    form: {
+        // Like <input type="text" name="name">
+        grant_type: 'authorization_code',
+        code: authCode,
+        redirect_uri: 'http://localhost:3000/tokencallback'
+    },
+    headers: {
+        /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
+    }
+  };
+
+  console.log("Posting to token endpoint");
+ 
+  var idToken = "";
+  var accessToken = "";
+  var tokenType = "";
+  var expiresIn = 0;
+  var bearer = "";
+  request(options)
+    .then(function (body) {
+        // POST succeeded...
+      console.log("post to token endpoint succeeded");
+      console.log(body);
+      var bodyJson = JSON.parse(body)
+      idToken = bodyJson.id_token;
+      accessToken = bodyJson.access_token;
+      tokenType = bodyJson.token_type;
+      expiresIn = bodyJson.expires_in;
+      bearer = "Bearer " + accessToken;
+    })
+    .catch(function (err) {
+        // POST failed...
+      console.log("post to token endpoint failed");
+      console.log(err);
+      return res.status(404).send();
+    }); 
+
+  console.log("Preparing userinfo get"); 
+  options = {
+    method: 'GET',
+    uri: idp + '/userinfo',
+    headers: {
+      'Authorization': bearer
+    }
+  };
+
+  /*options = {
+    method: 'GET',
+    uri: idp + '/userinfo',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  };*/
+  request(options)
+    .then(function (body) {
+      console.log("retrieval of userinfo endpoint succeeded");
+      console.log(body);
+      return res.status(200).send(body)
+    })
+    .catch(function (err) {
+      console.log("retrieval of userinfo endpoint failed");
+      console.log(err);
+      return res.status(404).send();
+    });  
+  
+  //return res.status(200).send("Ok");
+
+  /*
+  // take nonce from cookie
+  //const nonce = req.signedCookies[nonceCookie];
+
+  // delete nonce
+  //delete req.signedCookies[nonceCookie];
+
+  // take ID Token posted by the user
+  const {id_token} = req.body;
+
+  // decode token
+  const decodedToken = jwt.decode(id_token, {complete: true});
+
+  // get key id
+  const kid = decodedToken.header.kid;
+  
+  // get public key
+  const client = jwksClient({
+    jwksUri: oidcProviderInfo['jwks_uri'],
+  });
+  client.getSigningKey(kid, (err, key) => {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+
+    // verify signature & decode token
+    const verifiedToken = jwt.verify(id_token, signingKey);
+    
+    // check audience, nonce, and expiration time
+    const {
+      nonce: decodedNonce,
+      aud: audience,
+      exp: expirationDate,
+      iss: issuer,
+    } = verifiedToken;
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    const expectedAudience = process.env.CLIENT_ID;
+    if (audience !== expectedAudience ||
+        decodedNonce !== nonce ||
+        expirationDate < currentTime ||
+        issuer !== oidcProviderInfo['issuer']) {
+      // send an unauthorized http status
+      return res.status(401).send();
+    }
+
+    req.session.decodedIdToken = verifiedToken;
+    req.session.idToken = id_token;
+
+    // send the decoded version of the ID Token
+    res.redirect('/profile');
+    */
 });
 
 app.get('/to-dos', async (req, res) => {
@@ -58,6 +223,18 @@ app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
+const {OIDC_PROVIDER} = process.env;
+const discEnd = 'https://'+OIDC_PROVIDER+'/.well-known/openid-configuration';
+//console.log("Discovery endpoint: " + discEnd);
+request(discEnd).then((res) => {
+  oidcProviderInfo = JSON.parse(res);
+  //console.log(oidcProviderInfo);
+  app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
+}).catch((error) => {
+  console.error(error);
+  console.error('Unable to get OIDC endpoints for '+OIDC_PROVIDER);
+  process.exit(1);
 });
+
